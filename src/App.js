9dd4d39d475ecb7945d35c62b6e3c00c6733f978
femzykel <femmzykel@gmail.com>
2020-05-28 21:50:17 +0100

    modified search UI

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
     this.state = {
       monsters: [
         {
-          id: 1,
+          id: '1',
           name: 'Leanne Graham',
           username: 'Bret',
           email: 'Sincere@april.biz',
@@ -33,7 +33,7 @@ class App extends Component {
           },
         },
         {
-          id: 2,
+          id: '2',
           name: 'Ervin Howell',
           username: 'Antonette',
           email: 'Shanna@melissa.tv',
@@ -56,7 +56,7 @@ class App extends Component {
           },
         },
         {
-          id: 3,
+          id: '3',
           name: 'Clementine Bauch',
           username: 'Samantha',
           email: 'Nathan@yesenia.net',
@@ -79,7 +79,7 @@ class App extends Component {
           },
         },
         {
-          id: 4,
+          id: '4',
           name: 'Patricia Lebsack',
           username: 'Karianne',
           email: 'Julianne.OConner@kory.org',
@@ -102,7 +102,7 @@ class App extends Component {
           },
         },
         {
-          id: 5,
+          id: '5',
           name: 'Chelsey Dietrich',
           username: 'Kamren',
           email: 'Lucio_Hettinger@annie.ca',
@@ -125,7 +125,7 @@ class App extends Component {
           },
         },
         {
-          id: 6,
+          id: '6',
           name: 'Mrs. Dennis Schulist',
           username: 'Leopoldo_Corkery',
           email: 'Karley_Dach@jasper.info',
@@ -148,7 +148,7 @@ class App extends Component {
           },
         },
         {
-          id: 7,
+          id: '7',
           name: 'Kurtis Weissnat',
           username: 'Elwyn.Skiles',
           email: 'Telly.Hoeger@billy.biz',
@@ -171,7 +171,7 @@ class App extends Component {
           },
         },
         {
-          id: 8,
+          id: '8',
           name: 'Nicholas Runolfsdottir V',
           username: 'Maxime_Nienow',
           email: 'Sherwood@rosamond.me',
@@ -194,7 +194,7 @@ class App extends Component {
           },
         },
         {
-          id: 9,
+          id: '9',
           name: 'Glenna Reichert',
           username: 'Delphine',
           email: 'Chaim_McDermott@dana.io',
@@ -217,7 +217,7 @@ class App extends Component {
           },
         },
         {
-          id: 10,
+          id: '10',
           name: 'Clementina DuBuque',
           username: 'Moriah.Stanton',
           email: 'Rey.Padberg@karina.biz',
